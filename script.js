bc8f45e9b149dbbdc0c0d69af30cc282965e536c
rodridelossantos <137809941+rodridelossantos@users.noreply.github.com>
2023-09-10 20:18:46 -0300

    add functions

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-const dropdown = document.getElementById('dropdown');
-const imageDiv = document.getElementById('image-container');
+const dropdown = document.getElementById("dropdown");
+const imageDiv = document.getElementById("image-container");
 
+let API_ID =
+  "live_3epzTEAQb7m3dLHYFZWgNlx7MuEmuyP6aTXuLhIKOB7yQLUBINIwpazCURpyvZ58";
+let breeds = [];
 
+const getCatBreeds = async () => {
+  const response = await fetch("https://api.thecatapi.com/v1/breeds");
+  const data = await response.json();
 
-let API_ID = ;
-let breeds = {};
+  return populateBreeds(data);
+};
 
-const getCatBreeds = () => {
-    fetch("https://api.thecatapi.com/v1/breeds").then((response) => response.json()).then((data) => )
-}
+const populateBreeds = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let { id, name } = arr[i];
+    let obj = { id: id, name: name };
+    breeds.push(obj);
+  }
+};
 
-const populateBreeds = () 
+document.addEventListener("DOMContentLoaded", () => {
+  getCatBreeds();
+});
